a16d6ee09e4412708abc29d996470da86ca4ad1b
Jenisp69 <jenispudasaini3@gmail.com>
2026-01-17 20:29:18 +0545

    added placeholder link on action bar and started js

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+    console.log("Welcome to Samrat Iron Website");
+    console.log("2+2");
+    console.warn("WARNING GAY BOI DETECTED");
+    console.error("ERROR 404 GIRLS Detected");
+    console.log("Made by jenis");
+    //console.clear();
